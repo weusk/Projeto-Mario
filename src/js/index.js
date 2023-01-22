83e25310d91bf49e582b9f38840a51ccf061faf6
weusk <122845538+weusk@users.noreply.github.com>
2023-01-21 22:08:29 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const video = document.getElementById("video");
+const botaoTrailer = document.querySelector(".botao-trailer")
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const linkDoVideo = video.src; 
+
+function alternarModal(){
+    modal.classList.toggle("aberto")
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () =>{
+    alternarModal();
+video.setAttribute("src", "")
+});
